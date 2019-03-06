0bbf44c17b1e3b8e8375bfcb9ee3a84c1b17a450
Nathan Cooper <nathan@lucidchart.com>
2019-03-06 13:10:01 -0700

    Fixed most of the simple logic
    Still a scary display of CSS

--- web/StarAuth/src/main/java/com/lucidctf/starauth/MvcConfig.java
+++ web/StarAuth/src/main/java/com/lucidctf/starauth/MvcConfig.java
@@ -13,8 +13,8 @@ public class MvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/home").setViewName("home");
-        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/home").setViewName("hello");
+        registry.addViewController("/").setViewName("hello");
         registry.addViewController("/hello").setViewName("hello");
         registry.addViewController("/login").setViewName("login");
     }
--- web/StarAuth/src/main/java/com/lucidctf/starauth/WebSecurityConfig.java
+++ web/StarAuth/src/main/java/com/lucidctf/starauth/WebSecurityConfig.java
@@ -17,7 +17,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
+                .antMatchers("/css/**", "/images/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
@@ -25,7 +25,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
             .logout()
-                .permitAll();
+                .permitAll()
+                .logoutSuccessUrl("/login?logout");
     }
 
     @Bean
@@ -33,8 +34,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public UserDetailsService userDetailsService() {
         UserDetails user =
              User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
+                .username("raynor")
+                .password("kerrigan")
                 .roles("USER")
                 .build();
 
